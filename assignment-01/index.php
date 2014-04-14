2136990170b5edd79d66f42ca6cd19bf243cb4ba
Minhchau Nguyen <cnguye63@seattlecentral.edu>
2014-04-14 14:20:13 -0700

    This is my assignment 01 - Git Busy

--- assignment-01/index.php
+++ assignment-01/index.php
@@ -1,4 +1,9 @@
-<!doctype html>
-<html>
-Hello world!!!
-</html>
+<?php
+include("header.php");
+?>
+<h2>Hello World</h2>
+<p>I really hope I wont have to see Putty ever again. It's so confusing.:( No offense.</p>
+
+<?php
+include ("footer.php");
+?>
