aef03a02026145af6f749c70c896dcf4b13946d0
Minhchau Nguyen <cnguye63@seattlecentral.edu>
2014-06-03 14:37:51 -0700

    Assignment 07

---
+++ assignment-07/list.php
@@ -0,0 +1,8 @@
+<style>
+li {text-align: left;}
+</style>
+<li>
+<!--<img src="images/divergent-trilogy.png" width="25%"/><br>-->
+<b><?=htmlentities($books["title"])?></b>--
+<i><?=htmlentities($books["author"])?></i>
+
