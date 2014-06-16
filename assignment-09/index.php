7335389d6a75384a1c35a6db898bbe52962b9c25
Minhchau Nguyen <cnguye63@seattlecentral.edu>
2014-06-15 19:56:46 -0700

    Assignent 9

---
+++ assignment-09/index.php
@@ -0,0 +1,20 @@
+<?php include("bus.php");
+
+$bus = new Bus;
+
+$bus->setSpeed(20);
+
+$bus->setSpeed(55);
+
+$bus->setSpeed(80);
+
+$bus->setSpeed(30); 
+
+$bus->trigger();
+
+$bus->getSpeed();
+?>
+<!doctype html>
+<body>
+<img src="joy9.gif";
+</body>
